f910493d9cc411b1c07a292a72fbcbaf1d0ca9cc
Johnny Mkhael <johnny.mkhael@gmail.com>
2017-11-11 17:28:13 +0100

    preparing faas image

--- monet.sh
+++ monet.sh
@@ -4,16 +4,32 @@ if [ -z "$IMAGE" ]; then
     IMAGE="content.jpg"
 fi
 
-mkdir -p out
+STYLE=$2
+
+if [ -z "$STYLE" ]; then
+    echo "STYLE is empty, defaulting to all"
 
 for i in $(seq 0 1 9);
-do
-  image_stylization_transform \
+  do
+   echo "Generating monet_styles $i on $IMAGE"
+   image_stylization_transform \
       --num_styles=10 \
       --checkpoint=/magenta-models/multistyle-pastiche-generator-monet.ckpt \
       --input_image=$IMAGE \
       --which_styles="{$i:1}" \
-      --output_dir=out \
+      --output_dir="out_""$IMAGE" \
       --output_basename="monet_styles"
-done
+   echo "done for style $i"
+  done
+else
+  echo "Generating monet_styles $STYLE on $IMAGE"
+  image_stylization_transform \
+      --num_styles=10 \
+      --checkpoint=/magenta-models/multistyle-pastiche-generator-monet.ckpt \
+      --input_image=$IMAGE \
+      --which_styles="{$STYLE:1}" \
+      --output_dir="out_""$IMAGE" \
+      --output_basename="monet_styles"
+  echo "done for style $STYLE"
 
+fi
--- varied.sh
+++ varied.sh
@@ -4,15 +4,33 @@ if [ -z "$IMAGE" ]; then
     IMAGE="content.jpg"
 fi
 
-mkdir -p out
+STYLE=$2
 
-for i in $(seq 0 1 31);
-do
-  image_stylization_transform \
+if [ -z "$STYLE" ]; then
+    echo "STYLE is empty, defaulting to all"
+
+  for i in $(seq 0 1 31);
+  do
+    echo "Generating varied_styles $i on $IMAGE"
+    image_stylization_transform \
       --num_styles=32 \
       --checkpoint=/magenta-models/multistyle-pastiche-generator-varied.ckpt \
       --input_image=$IMAGE \
       --which_styles="{$i:1}" \
-      --output_dir=out \
+      --output_dir="out_""$IMAGE" \
       --output_basename="varied_styles"
-done
+    echo "done for style $i"
+  done
+
+else
+  echo "Generating varied_styles $STYLE on $IMAGE"
+  image_stylization_transform \
+      --num_styles=32 \
+      --checkpoint=/magenta-models/multistyle-pastiche-generator-varied.ckpt \
+      --input_image=$IMAGE \
+      --which_styles="{$STYLE:1}" \
+      --output_dir="out_""$IMAGE" \
+      --output_basename="varied_styles"
+  echo "done for style $STYLE"
+
+fi
