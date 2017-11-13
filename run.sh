211bb2f0767f9e650b1b4db37a996e0fb4cf7c57
Johnny Mkhael <johnny.mkhael@gmail.com>
2017-11-13 14:28:12 +0100

    Muting Tensorflow warnings. Adding more images

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it -p 6006:6006 -v /home/johnny/workspace/faas-stylist:/magenta-data jmkhael/faas-stylist $1 $2 $3 $4 $5 $6 $7 $8 $9
+docker run -it -p 6006:6006 -e TF_CPP_MIN_LOG_LEVEL="3" -v /home/johnny/workspace/faas-stylist:/magenta-data jmkhael/faas-stylist $1 $2 $3 $4 $5 $6 $7 $8 $9
